af83baa61fe0ac84ac4ce556f6fe40433077262a
Martin Ankerl <martin.ankerl@gmail.com>
2017-01-24 06:38:46 +0100

    parsing toml

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,63 @@
-use std::io::{BufRead, BufReader, Write};
+extern crate regex;
+extern crate toml;
+
+use std::io::{BufRead, BufReader, Write, Read};
 use std::process::{Command, Stdio};
 use std::thread;
+use std::fs::File;
 use std::env;
+use regex::Regex;
 
-fn handle<W: Write>(out: &mut W, line: String) {
-    writeln!(out, "\x1B[33m{}\x1B[0m", line).unwrap();
+
+fn parse(filename: &str) {
+    let mut input = String::new();
+    File::open(&filename).and_then(|mut f| {
+            f.read_to_string(&mut input)
+    }).unwrap();    
+
+    let mut parser = toml::Parser::new(&input);
+    let toml = match parser.parse() {
+        Some(toml) => toml,
+        None => {
+            for err in &parser.errors {
+                let (loline, locol) = parser.to_linecol(err.lo);
+                let (hiline, hicol) = parser.to_linecol(err.hi);
+                println!("{}:{}:{}-{}:{} error: {}",
+                         filename, loline, locol, hiline, hicol, err.desc);
+            }
+            return
+        }
+    };
+    println!("{:?}", toml);
 }
 
+
+fn handle<W: Write>(out: &mut W, re: &Regex, line: String) {
+    if re.is_match(&line) {
+        writeln!(out, "\x1B[33m{}\x1B[0m", line).unwrap();
+    } else {
+        writeln!(out, "{}", line).unwrap();
+    }
+}
+
+/*
+#[derive(RustcDecodable, RustcEncodable)]
+pub struct Rules  {
+    cmd: String,
+    rules: Vec<Vec<String>>,
+}
+*/
+
 fn main() {
+    parse("test.toml");
+
+
+    // Serialize using `json::encode`
+    //let encoded = json::as_pretty_json(&o);
+    //println!("{}", encoded);
+
+    let re = Regex::new(r"h\wme").unwrap();
+
     let mut args: Vec<String> = env::args().collect();
     args.remove(0);
     let cmd: String = args.remove(0);
@@ -21,12 +71,13 @@ fn main() {
 
     // process stderr in other thread
     let mut child_stderr = child.stderr;
+    let r = re.clone();
     let stderr_thread = thread::spawn(move || {
         if let Some(ref mut stderr) = child_stderr {
             let self_stderr = std::io::stderr();
             let mut h = self_stderr.lock();
             for line in BufReader::new(stderr).lines() {
-                handle(&mut h, line.unwrap());
+                handle(&mut h, &r, line.unwrap());
             }
         }
         return child_stderr;
@@ -36,7 +87,7 @@ fn main() {
         let self_stdout = std::io::stdout();
         let mut h = self_stdout.lock();
         for line in BufReader::new(stdout).lines() {
-            handle(&mut h, line.unwrap());
+            handle(&mut h, &re, line.unwrap());
         }
     }
 
